d12a807bdae761681a279993030b8837e4e76de4
Kaveri Bhoj <129531779+16-kaveri@users.noreply.github.com>
2023-11-18 15:58:03 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -2,6 +2,6 @@ age=int(input("Enter your age:"))
 if age<18:
    print("You are minor:")
 elif age<65:
-   print("Uou are adult:")
+   print("You are adult:")
 else:
    print("You are senior citizen:")
